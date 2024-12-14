d1de8f1132b8d4b8e85d4887cd6fc7254659aa0f
Your GitHub Username <Your GitHub Email>
2024-12-14 14:36:33 +0100

    third commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Body from "../components/Body";
 import Footer from "../components/Footer";
 const Home = () => {
   return (
-    <div className="flex flex-col h-[100vh] justify-between relative overflow-hidden">
+    <div className="flex flex-col h-[100svh] justify-between relative overflow-hidden">
       <Header />
       <Body />
       <Footer/>
