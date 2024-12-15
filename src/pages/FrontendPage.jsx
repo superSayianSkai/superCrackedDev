b4c8431873cbe8fad5f7f2bc15594a797284b43c
Your GitHub Username <Your GitHub Email>
2024-12-15 02:29:11 +0100

    jsjsjs

--- src/pages/FrontendPage.jsx
+++ src/pages/FrontendPage.jsx
@@ -15,13 +15,13 @@ const FrontendPage = () => {
           key={index}
           className="border-[1px]  border-black border-t-0 border-r-0 border-l-0 flex flex-col  pb-[2rem] mx-8 my-8 cursor-pointer"
         >
-          <h2 className="font-bold my-[30px] text-[1rem]  md:text-[1.5rem]  text-[#7E57C2] ">
+          <h2 className="font-bold my-[30px] text-[1.4rem]  md:text-[1.5rem]  text-[#7E57C2] ">
             {items.week}: {items.weekGoal}
           </h2>
-          <ul className="list-disc mx-6  flex flex-col gap-6 text-[.8rem] md:text-[1rem] text-gray-800 ">
+          <ul className="list-disc mx-6  flex flex-col gap-6 text-[1rem]  text-gray-800 ">
             {items.resources.map((resource, index) => (
               <a key={index} href={resource.link} className="">
-                <li className="hover:bg-[#f7def7] rounded-lg p-2">
+                <li className="hover:bg-[#f1d9f1c4] rounded-lg p-2">
                   {resource.resource}
                 </li>
               </a>
--- src/pages/FullstackProjects.jsx
+++ src/pages/FullstackProjects.jsx
@@ -8,13 +8,13 @@ const FullstackProjects = () => {
         <img src={fullstack}   loading="lazy" className="w-[100%] h-[100%] object-cover" />
       </div>
 
-      <h2 className="font-bold my-[30px] text-[1rem]  md:text-[1.5rem]  text-[#7E57C2]  mx-8  ">
+      <h2 className="font-bold my-[30px] text-[1.4rem]  md:text-[1.5rem]  text-[#7E57C2]  mx-8  ">
         Fullstack Projects
       </h2>
       {fullstackProjects.map((items, index) => (
         <ul
           key={index}
-          className="list-disc  flex flex-col gap-10 text-[.8rem] md:text-[1rem] text-gray-800 cursor-pointer mx-20 my-8 "
+          className="list-disc  flex flex-col gap-10 text-[1rem] text-gray-800 cursor-pointer mx-20 my-8 "
         >
           <a key={index} href={items.link}  className="">
             <li className="hover:bg-[#f7def7] rounded-lg p-2 mb-5">
